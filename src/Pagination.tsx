11a31436ce8b0e5d98e35c3232bfa808a5cfae5c
lhh <jyslshsgdy@gmail.com>
2020-12-01 15:00:22 +0900

    button event 추가

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -51,41 +51,64 @@ const STDPageButton = styled.button`
   width: 50px;
   height: 35px;
   ${({ current }: { current: boolean }) => css`
-    ${current && css`background-color: blue;`}
+    ${current && css`background-color: cyan;`}
   `};
 
   & + & {
     margin-left: 10px;
   }
+
+  &:hover {
+    background-color: skyblue;
+  }
 `;
 
 const Pagination = () => {
   const LIMIT = 10;
+  const PAGELIMIT = 5;
   const [offset, setOffset] = useState(0);
   const [items, setItems] = useState<Item[]>([]);
   const [pages, setPages] = useState<Page[]>([]);
-
-  const btnOnClick = (num: number) => {
-    setPages(pages.map(el => ({...el, current: el.value === num ? true : false})));
-    setOffset((num - 1) * LIMIT);
-  }
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    const totalPages = Math.ceil(items.length / LIMIT);
     const pagesArr: Page[] = [];
     const itemsArr: Item[] = [];
 
-    for(let i = 1; i <= totalPages; i++) {
-      pagesArr.push({value: i, current: i === 1 ? true : false});
+    for(let i = 1; i <= PAGELIMIT; i++) {
+      pagesArr.push({ value: i, current: i === 1 ? true : false });
     }
 
     for(let i = 0; i < 100; i++) {
-      itemsArr.push({id: i, contents: i});
+      itemsArr.push({ id: i, contents: i });
     }
 
     setPages(pagesArr);
     setItems(itemsArr);
   }, []);
+
+  const btnOnClick = (num: number) => {
+    setPages(pages.map(el => ({ ...el, current: el.value === num ? true : false })));
+    setOffset((num - 1) * LIMIT);
+    setCurrentPage(num);
+  }
+
+  const btnDirectOnClick = (dir: string) => {
+    let val = 0;
+    if(dir === '<' && pages[0].value > 1) {
+      setPages(pages.map(el => ({ ...el, value: el.value - 1 })));
+    }
+    else if(dir === '>' && pages[PAGELIMIT - 1].value < Math.ceil(items.length / LIMIT)) {
+      setPages(pages.map(el => ({ ...el, value: el.value + 1 })));
+    }
+    else if(dir === '<<' && pages[0].value > 1) {
+      setPages(pages.map(el => ({ ...el, value: ++val })));
+    }
+    else if(dir === '>>' && pages[PAGELIMIT - 1].value < Math.ceil(items.length / LIMIT)) {
+      val = Math.ceil(items.length / LIMIT) - PAGELIMIT;
+      setPages(pages.map(el => ({ ...el, value: ++val })));
+    }
+  }
 
   return (
     <STDContainer>
@@ -97,11 +120,11 @@ const Pagination = () => {
         })}
       </STDItemsContainer>
       <STDPageBurronContainer>
-        <STDPageButton current={false}>{'<<'}</STDPageButton>
-        <STDPageButton current={false}>{'<'}</STDPageButton>
+        <STDPageButton current={false} onClick={() => btnDirectOnClick('<<')}>{'<<'}</STDPageButton>
+        <STDPageButton current={false} onClick={() => btnDirectOnClick('<')}>{'<'}</STDPageButton>
         {pages.map(el => (<STDPageButton key={el.value} current={el.current} onClick={() => btnOnClick(el.value)}>{el.value}</STDPageButton>))}
-        <STDPageButton current={false}>{'>'}</STDPageButton>
-        <STDPageButton current={false}>{'>>'}</STDPageButton>
+        <STDPageButton current={false} onClick={() => btnDirectOnClick('>')}>{'>'}</STDPageButton>
+        <STDPageButton current={false} onClick={() => btnDirectOnClick('>>')}>{'>>'}</STDPageButton>
       </STDPageBurronContainer>
     </STDContainer>
   );
