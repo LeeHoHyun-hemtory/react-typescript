c660f85dea06f720bafb8363039fb6bc43d9a8cc
jyslshsgdy@gmail.com-at-600018716867 <jyslshsgdy@gmail.com>
2021-01-20 19:10:59 +0900

    Router 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Main from './Main';
 import Pagination from './Pagination';
 import FireBaseEX from './FireBaseEX';
+import Test from './Test';
+import Slider from './Slider';
+import InfiniteSlider from './InfiniteSlider';
 
 function App() {
   return (
-    <>
-      <Pagination />
-      {/* <FireBaseEX /> */}
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Main} exact/>
+        <Route path='/pagination' component={Pagination}/>
+        <Route path='/fireBaseEx' component={FireBaseEX}/>
+        <Route path='/Slider' component={Slider}/>
+        <Route path='/infiniteSlider' component={InfiniteSlider}/>
+        <Route path='/test' component={Test}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
