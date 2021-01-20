bc350f934e535739a012e487f7b67ada34babf90
jyslshsgdy@gmail.com-at-600018716867 <jyslshsgdy@gmail.com>
2021-01-20 19:32:34 +0900

    Main component 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import './App.css';
-import Main from './Main';
 import Pagination from './Pagination';
 import FireBaseEX from './FireBaseEX';
 import Test from './Test';
 import Slider from './Slider';
 import InfiniteSlider from './InfiniteSlider';
+import mixin from './styles/mixin';
+
+const SCContainer = styled.div`
+  height: 100vh;
+  ${mixin.flexSet()};
+`;
 
 function App() {
   return (
     <BrowserRouter>
+      <SCContainer>
+        <Link to='/pagination'>pagination</Link>
+        <Link to='/fireBaseEx'>fireBaseEx</Link>
+        <Link to='/Slider'>Slider</Link>
+        <Link to='/infiniteSlider'>infiniteSlider</Link>
+        <Link to='/test'>test</Link>
+      </SCContainer>
+      
       <Switch>
-        <Route path='/' component={Main} exact/>
         <Route path='/pagination' component={Pagination}/>
         <Route path='/fireBaseEx' component={FireBaseEX}/>
         <Route path='/Slider' component={Slider}/>
