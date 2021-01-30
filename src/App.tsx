07c38d422302e0993551a6d6292a6f865c2e02e7
jyslshsgdy@gmail.com-at-600018716867 <jyslshsgdy@gmail.com>
2021-01-30 17:35:06 +0900

    route 파일 import로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import styled from 'styled-components';
 import './App.css';
-import Pagination from './Pagination';
-import FireBaseEX from './FireBaseEX';
-import Test from './Test';
-import Slider from './Slider';
-import InfiniteSlider from './InfiniteSlider';
-import mixin from './styles/mixin';
-
-const SCContainer = styled.div`
-  height: 100vh;
-  ${mixin.flexSet()};
-`;
+import Routes from './Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <SCContainer>
-        <Link to='/pagination'>pagination</Link>
-        <Link to='/fireBaseEx'>fireBaseEx</Link>
-        <Link to='/Slider'>Slider</Link>
-        <Link to='/infiniteSlider'>infiniteSlider</Link>
-        <Link to='/test'>test</Link>
-      </SCContainer>
-      
-      <Switch>
-        <Route path='/pagination' component={Pagination}/>
-        <Route path='/fireBaseEx' component={FireBaseEX}/>
-        <Route path='/Slider' component={Slider}/>
-        <Route path='/infiniteSlider' component={InfiniteSlider}/>
-        <Route path='/test' component={Test}/>
-      </Switch>
-    </BrowserRouter>
+    <Routes />
   );
 }
 
