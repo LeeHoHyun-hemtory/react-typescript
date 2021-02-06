37274867f8da30845cc1a5d74903a854e8f3d124
jyslshsgdy@gmail.com-at-600018716867 <jyslshsgdy@gmail.com>
2021-02-06 17:41:57 +0900

    무한 스크롤 기초 작업

--- src/pages/infiniteScroll/InfiniteScroll.tsx
+++ src/pages/infiniteScroll/InfiniteScroll.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import Nav from '../../component/nav/Nav';
 import mixin from '../../styles/mixin';
 
+interface IImage {
+  id: number;
+  url: string;
+}
+
+const SCContainer = styled.div`
+  ${mixin.flexSet()};
+`;
+
+const SCScrollContainer = styled.div`
+  width: 60%;
+`;
+
+const SCImagesContainer = styled.div`
+  width: 100%;
+`;
+
+const SCImage = styled.img`
+  width: 100%;
+  height: 600px;
+`;
+
 const InfiniteScroll = () => {
+  const LIMIT = 4;
+  const [images, SetImages] = useState<IImage[]>([]);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const imgArr = [
+      {id: offset + 0, url: 'https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg'},
+      {id: offset + 1, url: 'https://www.ui4u.go.kr/depart/img/content/sub03/img_con03030100_01.jpg'},
+      {id: offset + 2, url: 'https://image-notepet.akamaized.net/resize/620x-/seimage/20191114%2F6a4c967c5b14197dd5d2c47424ae8e82.jpg'},
+      {id: offset + 3, url: 'https://newsimg.hankookilbo.com/cms/articlerelease/2020/09/13/c22a51dd-d1f8-48d2-82ad-f18a671078be.jpg'}
+    ];
+
+    SetImages(imgArr);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      console.log(window.pageYOffset, window.innerHeight);
+    });
+  }, [])
+
   return (
-    <div>
+    <SCContainer>
       <Nav />
-      헬로
-    </div>
+      <SCScrollContainer>
+        <SCImagesContainer>
+          {images.map(el => <SCImage key={el.id} src={el.url} />)}
+        </SCImagesContainer>
+      </SCScrollContainer>
+    </SCContainer>
   );
 };
 
