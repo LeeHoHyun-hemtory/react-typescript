41445be6a6bd051a3ed7c2c70a07fc6c787cc861
LeeHoHyun-hemtory <jyslshsgdy@gmail.com>
2021-02-23 18:25:25 +0900

    AutoSlider page 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import styled from 'styled-components';
 import Pagination from './pages/pagination/Pagination';
 import FireBaseEX from './pages/firebase/FireBaseEX';
 import Test from './pages/test/Test';
 import Slider from './pages/slider/Slider';
 import InfiniteSlider from './pages/infiniteSlider/InfiniteSlider';
 import InfiniteScroll from './pages/infiniteScroll/InfiniteScroll';
+import AutoSlider from './pages/autoSlider/AutoSlider';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Pagination}/>
+        <Route path='/pagination' exact component={Pagination}/>
         <Route path='/fireBaseEx' component={FireBaseEX}/>
-        <Route path='/Slider' component={Slider}/>
+        <Route path='/slider' component={Slider}/>
         <Route path='/infiniteSlider' component={InfiniteSlider}/>
         <Route path='/infiniteScroll' component={InfiniteScroll}/>
+        <Route path='/autoSlider' component={AutoSlider}/>
         <Route path='/test' component={Test}/>
       </Switch>
     </BrowserRouter>
